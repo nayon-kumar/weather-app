bb83c2da598ffe139c0c72db5d6c0cd498b7c424
nayon-kumar <nayon.cse55@gmail.com>
2026-03-21 21:22:40 +0600

    UI design started

---
+++ src/components/WeatherContainer/WeatherContainer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const WeatherContainer = () => {
+  return (
+    <div className="bg-blue-800 text-white max-w-[95%] md:max-w-175 mx-auto p-5 rounded-2xl">
+      <h1>Weather container</h1>
+    </div>
+  );
+};
+
+export default WeatherContainer;
