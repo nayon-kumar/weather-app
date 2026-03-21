366c5e09c7a1c7a1f9d86e2aedbd504ca0ee2458
nayon-kumar <nayon.cse55@gmail.com>
2026-03-21 21:51:29 +0600

    Search option added

--- src/components/WeatherContainer/WeatherContainer.jsx
+++ src/components/WeatherContainer/WeatherContainer.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { Search } from "lucide-react";
 
 const WeatherContainer = () => {
   return (
-    <div className="bg-blue-800 text-white max-w-[95%] md:max-w-175 mx-auto p-5 rounded-2xl">
-      <h1>Weather container</h1>
+    <div className="bg-blue-800 text-white max-w-[95%] sm:max-w-150 mx-auto px-3 py-10 rounded-2xl">
+      <div className="flex items-center justify-center gap-2">
+        <input
+          className="bg-white rounded-full text-gray-500 py-2 px-5"
+          type="text"
+          placeholder="Search"
+        />
+        <div>
+          <Search size={30} className="cursor-pointer" />
+        </div>
+      </div>
     </div>
   );
 };
