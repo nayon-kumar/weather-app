b0aac1fec4a25793b1e49b2394da9f9c41347adb
nayon-kumar <nayon.cse55@gmail.com>
2026-03-22 22:06:24 +0600

    Some issue fixed

--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -1,37 +1,60 @@
 import React from "react";
 import clear from "../../assets/Weather/clear.png";
+import cloud from "../../assets/Weather/cloud.png";
+import rain from "../../assets/Weather/rain.png";
 
-const Weather = ({ weatherData, city }) => {
+const Weather = ({ weatherData, myCity }) => {
+  console.log(weatherData);
   return (
     <div className="mt-5">
       <div className="flex flex-col items-center">
-        <img className="mx-auto h-30 sm:h-40" src={clear} alt="" />
+        <img
+          className="mx-auto h-30 sm:h-40"
+          src={
+            weatherData?.description === "Partly cloudy"
+              ? cloud
+              : weatherData?.description === "Light rain shower"
+                ? rain
+                : weatherData?.description === "Patchy rain nearby"
+                  ? rain
+                  : clear
+          }
+          alt={weatherData?.description}
+        />
         <p>{weatherData?.description}</p>
       </div>
-      <div className="text-center mt-3">
-        <h1 className="text-3xl sm:text-4xl font-bold">
-          {weatherData?.temperature}
-        </h1>
-        <h3 className="text-2xl sm:text-2.5xl mt-2 font-semibold">
-          {city.charAt(0).toUpperCase() + city.slice(1)}
-        </h3>
-        <p className="mt-2">Wind Spreed: {weatherData?.wind}</p>
-      </div>
-      <h3 className="text-center my-5 text-2xl font-bold">Forecasts</h3>
-      <div className="grid grid-cols-3 items-center justify-between gap-3">
-        {weatherData?.forecast.map((data, index) => {
-          return (
-            <div
-              key={data.day}
-              className="bg-[#f39c12] p-2 rounded-md text-center"
-            >
-              <h3>Day {data.day}</h3>
-              <h1 className="text-xl font-bold">{data.temperature}</h1>
-              <h3 className="font-bold">{data.wind}</h3>
-            </div>
-          );
-        })}
-      </div>
+      {weatherData === null ? (
+        <>
+          <h3 className="text-center">Please search by a city name</h3>
+        </>
+      ) : (
+        <>
+          <div className="text-center mt-3">
+            <h1 className="text-3xl sm:text-4xl font-bold">
+              {weatherData?.temperature}
+            </h1>
+            <h3 className="text-2xl sm:text-2.5xl mt-2 font-semibold">
+              {myCity.charAt(0).toUpperCase() + myCity.slice(1)}
+            </h3>
+            <p className="mt-2">Wind Spreed: {weatherData?.wind}</p>
+          </div>
+          <h3 className="text-center my-5 text-2xl font-bold">Forecasts</h3>
+          <div className="grid grid-cols-3 items-center justify-between gap-3">
+            {weatherData?.forecast.map((data, index) => {
+              return (
+                <div
+                  key={data.day}
+                  className="bg-[#f39c12] p-2 rounded-md text-center"
+                >
+                  <h3>Day {data.day}</h3>
+                  <h1 className="text-xl font-bold">{data.temperature}</h1>
+                  <h3 className="font-bold">{data.wind}</h3>
+                </div>
+              );
+            })}
+          </div>
+        </>
+      )}
     </div>
   );
 };
--- src/components/WeatherContainer/WeatherContainer.jsx
+++ src/components/WeatherContainer/WeatherContainer.jsx
@@ -6,20 +6,27 @@ import axios from "axios";
 const WeatherContainer = () => {
   const [weatherData, setWeatherData] = useState(null);
   const [city, setCity] = useState("");
+  const [myCity, setMyCity] = useState("");
+  const [error, setError] = useState("");
 
   const featchWeather = async (city = "Dhaka") => {
     try {
+      setError("");
       const res = await axios.get(`https://goweather.xyz/weather/${city}`);
       setWeatherData(res.data);
+      setMyCity(city);
     } catch (error) {
-      console.log(error);
+      setWeatherData(null);
+      setError(`${city} city not found. Please try another city!`);
     }
   };
 
   return (
     <div className="bg-blue-800 text-white max-w-[95%] sm:max-w-150 mx-auto px-5 sm:px-10 py-10 sm:py-20 rounded-2xl">
       <SearchBar featchWeather={featchWeather} city={city} setCity={setCity} />
-      <Weather weatherData={weatherData} city={city} />
+      <Weather weatherData={weatherData} myCity={myCity} />
+
+      {error && <p className="text-red-300 text-center mt-4">{error}</p>}
     </div>
   );
 };
